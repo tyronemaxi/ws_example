5045be7522690128a99007a25a4de6360debbf6d
田志超 <tyronextian@gmail.com>
2024-10-16 14:12:23 +0800

    [FEATURE] 项目初始化

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-REPOSITORY_NAME=10.29.40.23:7098/ai/docker-prod/
+REPOSITORY_NAME=tyronextian
 IMAGE_NAME=debian_python_base
 TAG=3.10.13
 
-docker build -t ${IMAGE_NAME}:${TAG} -f base_v2.Dockerfile .
-docker tag ${IMAGE_NAME}:${TAG} ${REPOSITORY_NAME}${IMAGE_NAME}:${TAG}
+docker build -t ${IMAGE_NAME}:${TAG} -f base.Dockerfile .
+docker tag ${IMAGE_NAME}:${TAG} ${REPOSITORY_NAME}/${IMAGE_NAME}:${TAG}
 #docker push ${IMAGE_NAME}:${TAG}
 
-echo "docker pull ${REPOSITORY_NAME}${IMAGE_NAME}:${TAG}"
+echo "docker pull ${REPOSITORY_NAME}/${IMAGE_NAME}:${TAG}"
